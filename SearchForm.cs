ab011b0269f15aabcda65ab7d514164ff69a6fd8
X00145679 <43677677+X00145679@users.noreply.github.com>
2020-12-14 14:47:19 +0000

    Revert "new site"
    This reverts commit dac083c5a74bad59880026822e8fda20e36f9b10.

---
+++ SearchForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CA3
+{
+    public class SearchForm
+    {
+        [Required]
+        [StringLength(18, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        
+        public bool critic { get; set; }
+
+    }
+}
